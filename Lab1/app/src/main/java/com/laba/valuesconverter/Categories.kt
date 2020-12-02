b9a0ba621711fd29ddf4696400962a13c06cd528
Alex Avetsiuk <gmltt@yandex.ru>
2020-12-02 13:27:03 +0300

    Revert "lab 1: value converter"

--- Lab1/app/src/main/java/com/laba/valuesconverter/Categories.kt
+++
@@ -1,9 +0,0 @@
-package com.laba.valuesconverter
-
-import androidx.annotation.StringRes
-
-enum class Categories(@StringRes val friendlyNameResId: Int) {
-    LENGTH(R.string.length),
-    TIME(R.string.time),
-    VOLUME(R.string.volume)
-}
